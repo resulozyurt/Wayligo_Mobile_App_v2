c17952ace1645ee91d7a12eca9c5f4d905d96aec
Resul Özyurt <rozyurt791@gmail.com>
2026-07-08 18:18:49 +0300

    backendllogin düzeltmesi

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
